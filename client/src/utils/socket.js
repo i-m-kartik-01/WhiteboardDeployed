6b93def5ddca78723affc11f82e0fc70d1bc9f33
i-m-kartik-01 <guptakartik2146@gmail.com>
2025-12-17 16:22:34 +0530

    entered backend URL everywhere

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5003", {
+const socket = io("https://whiteboarddeployed.onrender.com", {
   withCredentials: true,
 });
 
